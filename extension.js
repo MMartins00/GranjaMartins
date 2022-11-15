f6e5527c7f167ec2647bfa05935939edcd2f4365
marcos martins <marcosgelp87@gmail.com>
2022-11-14 21:27:42 -0300

    first commit

--- extension.js
+++ extension.js
@@ -5,11 +5,14 @@ function calcularPrecio(precioMaple, cantidadMaple, porcentajedescuento) {
     return (preciocondescuento * cantidadMaple);
 }
 //le pedimos al cliente que ingrese que tipo de huevos va comprar
+let entrada 
 do{
 let color = prompt("ingrese el tipo de huevos que quiere comprar");
-switch (color == 1 || color == 2){
-case 1: { alert("el precio de los huevos blancos es $700");}
-case 2: {alert("el precio de los huevos colorados es $750") };}
+if (color == 1) {alert("el precio de los huevos blancos es $700");
+    
+} else {
+    alert("el precio de los huevos colorados es $750")
+}
 
 let precioMaple = parseInt(prompt("ingrese el precio del maple"))
 
@@ -18,7 +21,7 @@ if (cantidadMaple <= 0) { alert("tienes que ingresar una cantidad valida"); }
 
 else if (cantidadMaple >= 12) { alert("tiene un descuento de 10%"); }
 let descuento = parseInt(prompt("ingrese el descuento (si corresponde)"))
+entrada = prompt("desea continuar comprando?")} while (entrada == "No")
 
 let preciofinal = calcularPrecio(precioMaple + cantidadMaple + descuento);
 alert("el precio final de su compra es $" + preciofinal);
-let entrada = prompt("desea continuar comprando?")} while(entrada !="No");
