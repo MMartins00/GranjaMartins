72c6e946242edbdbf693b4cb9a76a650709eb5d0
marcos martins <marcosgelp87@gmail.com>
2022-12-24 00:13:08 -0300

    tercer entrega

--- extension.js
+++ extension.js
@@ -1,53 +1,25 @@
-//mi idea de proyecto final sera un "ecommerce" de huevos
-function calcularPrecio(precioMaple, cantidadMaple, porcentajedescuento) {
-    let descuento = (precioMaple * porcentajedescuento) / 100;
-    let preciocondescuento = precioMaple - descuento;
-    return (preciocondescuento * cantidadMaple);
-}
-//le pedimos al cliente que ingrese que tipo de huevos va comprar
-let entrada 
-do{
-let color = prompt("ingrese el tipo de huevos que quiere comprar");
-if (color == 1) {alert("el precio de los huevos blancos es $700");
-    
-} else {
-    alert("el precio de los huevos colorados es $750")
-}
-
-let precioMaple = parseInt(prompt("ingrese el precio del maple"))
-
-let cantidadMaple = parseInt(prompt("ingrese la cantidad de maples que desea"));
-if (cantidadMaple <= 0) { alert("tienes que ingresar una cantidad valida"); }
-
-else if (cantidadMaple >= 12) { alert("tiene un descuento de 10%"); }
-let descuento = parseInt(prompt("ingrese el descuento (si corresponde)"))
-entrada = prompt("desea continuar comprando?")} while (entrada === "No")
-
-let preciofinal = calcularPrecio(precioMaple + cantidadMaple + descuento);
-alert("el precio final de su compra es $" + preciofinal);
-
-
-class Huevos {
+class producto {
     constructor(Color, precio) {
         this.Color = Color;
         this.precio = precio;
         this.cantidad = 200;
     }
-    Vender() {
-        if(this.cantidad > 0) {
-            this.cantidad --;
-        }
-    else {
-        console.log("No hay mas stock");
-    }
-}}
+    
+}
 
-const colorados = new huevos(colorados, 750);
-const blancos = new huevos(blancos, 700);
+const colorados = new producto("colorados", 750);
+const blancos = new producto("blancos", 700);
 
-const stockhuevos = [Colorados, Blancos];
+const stockhuevos = [colorados, blancos];
 console.log(stockhuevos);
-
 for(let producto of stockhuevos) {
     console.log(producto)
-}
+};
+const arrayproductos = [colorados, blancos];
+const productos = document.getElementById("productos");
+arrayproductos.forEach( producto => {
+    let div = document.createElement("div");
+    div.innerHTML = ` <p> ${producto.Color} </p> <p> precio: ${producto.precio} </p>
+    <button> agregar al carrito</button> `
+    productos.appendChild(div);
+})
