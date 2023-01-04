bb9e8a4b0ebd5d045ddcc9dbd8d578c185f175c9
marcos martins <marcosgelp87@gmail.com>
2023-01-04 20:16:45 -0300

    entregafinal

--- extension.js
+++ extension.js
@@ -1,25 +1,40 @@
 class producto {
-    constructor(Color, precio) {
-        this.Color = Color;
+    constructor(nombre, precio, id) {
+        this.nombre = nombre;
         this.precio = precio;
-        this.cantidad = 200;
+        this.id = id;
+        this.cantidad = 1;
     }
-    
+
 }
 
-const colorados = new producto("colorados", 750);
-const blancos = new producto("blancos", 700);
+const coloradon1 = new producto("colorado n 1", 750, 1);
+const coloradon2 = new producto("colorado n 2", 650, 2);
+const coloradosuper = new producto("colorado super", 850, 3);
+const blancon1 = new producto("blanco n 1", 700, 4);
+const blancon2 = new producto("blanco n 2", 600, 5);
+const blancosuper = new producto("blanco super", 800, 6);
 
-const stockhuevos = [colorados, blancos];
-console.log(stockhuevos);
-for(let producto of stockhuevos) {
-    console.log(producto)
-};
-const arrayproductos = [colorados, blancos];
+//Declaramos el carrito:
+
+let carrito= [];
+
+
+const arrayproductos = [coloradon1, coloradon2, coloradosuper, blancon1, blancon2, blancosuper];
 const productos = document.getElementById("productos");
-arrayproductos.forEach( producto => {
-    let div = document.createElement("div");
-    div.innerHTML = ` <p> ${producto.Color} </p> <p> precio: ${producto.precio} </p>
-    <button> agregar al carrito</button> `
-    productos.appendChild(div);
-})
+const verproductos = () => {
+arrayproductos.forEach(producto => {
+    const div1 = document.createElement("div");
+    div1.classList.add("col-xl-3", "col-md-6", "col-sm-12");
+    div1.innerHTML = `<div>
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"> ${producto.nombre}</h5>
+      <p class="card-text"> precio: ${producto.precio}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+      
+ `
+    productos.appendChild(div1);
+})}
